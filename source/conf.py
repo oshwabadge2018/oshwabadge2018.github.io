dc4c9d128db885db99fbe44b07a1e6f8fb4aae90
Alex Camilo <alex.camilo@gmail.com>
2018-08-03 03:42:15 -0400

    Removed Theme options dict

--- source/conf.py
+++ source/conf.py
@@ -97,21 +97,6 @@ html_static_path = ['_static']
 #
 # html_sidebars = {}
 
-html_theme_options = {
-    'canonical_url': '',
-    'analytics_id': '',
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
