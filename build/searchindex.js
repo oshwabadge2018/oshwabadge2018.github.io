d39e9d132795f89f32d7bf13ce04627cf7a6eaca
Alex Camilo <alex.camilo@gmail.com>
2018-08-03 02:04:56 -0400

    Sphynx and Travis CI integration

---
+++ build/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to oshwabadge2018\u2019s documentation!"],titleterms:{document:0,indic:0,oshwabadge2018:0,tabl:0,welcom:0}})
